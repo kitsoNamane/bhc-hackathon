85c5acc9f3a4033bd703717d3ef7a195733cb7e2
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-06-25 13:24:16 +0200

    feat: implement fault reporting

--- lib/view/service_success.dart
+++ lib/view/service_success.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../view_model/app_state.dart';
 
 class ServiceSuccessPage extends StatelessWidget {
   const ServiceSuccessPage({super.key});
@@ -9,9 +12,123 @@ class ServiceSuccessPage extends StatelessWidget {
       appBar: AppBar(
         title: null,
       ),
-      body: const Center(
-        child: Text("Fault Created Successfully"),
+      body: Center(
+        child: Consumer<ApplicationState>(
+            builder: (context, state, child) => Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primary,
+                      width: 1
+                    )
+                  ),
+                  child: Icon(
+                    _ticketIcon(state.currentFault!.type!),
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 64,
+                  ),
+                ),
+                Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        style: BorderStyle.solid,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text("BHC00000${state.currentFault?.id}",
+                              style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 3),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Plot Number: ${state.currentFault?.plotNumber}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Type: ${state.currentFault?.type}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Incident: ${state.currentFault?.incident}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Due Date: ${state.currentFault?.incident}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Severity: ${state.currentFault?.severity}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Status: ${state.currentFault?.status}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          )
       ),
     );
+  }
+
+  IconData _ticketIcon(String type) {
+    return switch (type.toLowerCase()) {
+      "electrical" => Icons.flash_off_outlined,
+      "carpentry" => Icons.carpenter_outlined,
+      "plumbing" => Icons.plumbing_outlined,
+      "masonry" => Icons.flood_outlined,
+      "mechanical" => Icons.ac_unit_outlined,
+      "external" => Icons.add_business_outlined,
+      String() => Icons.electric_bolt_outlined,
+    };
   }
 }
