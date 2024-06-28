d5d990180cbc146ed245d11e5b805195f8981bf3
0221PHENYOgit config --global user.email phenyoothusitse45@gmail.com <abc20-013@thuto.bac.ac.bwgit config --global user.email abc20-013@thuto.bac.ac.bw>
2024-06-28 14:25:28 +0200

    Int

--- lib/view/faq.dart
+++ lib/view/faq.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class FAQPage extends StatelessWidget {
   const FAQPage({super.key});
+
+  
 @override
   Widget build(BuildContext context) {
     return Scaffold(
